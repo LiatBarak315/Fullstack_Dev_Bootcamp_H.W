6c7992a4fbd65c44a3fc06c621a23c4f92228960
LiatBarak315 <li.lev315@yahoo.co.il>
2021-11-07 15:24:24 +0200

    first commit

---
+++ Javaskript/1.1_primitive_data_types/1.1_primitive_data_types.js
@@ -0,0 +1,10 @@
+let MyString="my name"
+console.log(typeof MyString);
+let num=4;
+console.log(typeof num);
+let car={tesla:3};
+console.log(typeof car);
+let winterSeason= false;
+console.log(typeof winterSeason);
+let my;
+console.log(typeof my);
