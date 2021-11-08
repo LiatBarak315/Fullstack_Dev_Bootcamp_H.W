0eecc504511e7556fa1973b2f0ac0e700e38ef8e
LiatBarak315 <li.lev315@yahoo.co.il>
2021-11-08 15:47:41 +0200

    finish

---
+++ Javascript/3.3_func_decl_vs_exp/3.3_func_decl_vs_exp.js
@@ -0,0 +1,10 @@
+function percentageOfWorld1(population){
+    const pn=7900;
+    return (population/pn)*100;
+}
+let China =percentageOfWorld1(1441);
+let U_S =percentageOfWorld1(329.5);
+let Israel =percentageOfWorld1(9.217);
+console.log(China);
+console.log(U_S);
+console.log(Israel);
