48deec17bf05d47f18077fba7d87ed35bf4ce24e
LiatBarak315 <li.lev315@yahoo.co.il>
2021-11-09 14:05:11 +0200

    finish

--- Javascript/4.5_leap_year/4.5_leap_year.js
+++ Javascript/4.5_leap_year/4.5_leap_year.js
@@ -1,9 +1,10 @@
 function leapYear(n) {
-  if (n % 4 && n % 100 && n % 400) {
+  if (n % 4===0 && n % 100===0 && n % 400===0) {
     console.log("It is indeed a leap year");
-  } else if (n % 4 && n % 100) {
+  } else if (n % 4===0 && n % 100===0) {
     console.log("This is not a leap year");
-  } else if (n % 4) {
+  } else if (n % 4===0) {
     console.log("It is indeed a leap year");
   } else console.log("This is not a leap year");
 }
+leapYear(2400)
