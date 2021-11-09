1a291d4b67f4004e3c7e6daacc973d47c295bd9d
LiatBarak315 <li.lev315@yahoo.co.il>
2021-11-09 10:03:49 +0200

    finish

--- Javascript/4.4_boom/4.4_boom.js
+++ Javascript/4.4_boom/4.4_boom.js
@@ -2,11 +2,9 @@ function Boom(n) {
   for (let i = 1; i <= n; i++) {
     if (i % 7 === 0 && i.toString().includes("7")) {
       console.log("boom-boom");
-      i++;
     } else if (i % 7 == 0) {
       console.log("boom");
-      i++;
-    }
-    console.log(i);
+    } else console.log(i);
   }
 }
+Boom(77);
