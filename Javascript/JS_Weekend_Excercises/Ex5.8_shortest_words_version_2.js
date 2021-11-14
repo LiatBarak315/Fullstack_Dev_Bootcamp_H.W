8360d68f341d1a997bfcafa77aed240de8eb8a40
LiatBarak315 <li.lev315@yahoo.co.il>
2021-11-14 10:38:28 +0200

    finish

--- Javascript/JS_Weekend_Excercises/Ex5.8_shortest_words_version_2.js
+++ Javascript/JS_Weekend_Excercises/Ex5.8_shortest_words_version_2.js
@@ -1,2 +1,12 @@
-const findShort = (n) => Math.max(...n.split(" ").map((x) => x.length));
-console.log(findShort("kjxlksjxlks smldkel ksowkjq gdgd kjojlm,nm"));
+const findShort = (n) => {
+  let words = n.split(" ");
+  let wordL = words.map((x) => x.length);
+  let maxI = Math.max(...wordL);
+  const result = [];
+  for (let i = 0; i != words.length; i++) {
+    if (wordL[i] == maxI) result.push(words[i]);
+  }
+  return result;
+};
+
+console.log(findShort("anan aaa aaa snsn zz rr rer"));
