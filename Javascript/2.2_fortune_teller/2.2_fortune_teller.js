2e5b1cc1b060397587af582c748c7c102bfbb968
LiatBarak315 <li.lev315@yahoo.co.il>
2021-11-16 15:22:38 +0200

    finish

--- Javascript/2.2_fortune_teller/2.2_fortune_teller.js
+++ Javascript/2.2_fortune_teller/2.2_fortune_teller.js
@@ -2,4 +2,4 @@ function tellFortune( jobTitle, location, partnerName, numberOfChildren){
     console.log(`You will be a ${jobTitle} in ${location} and married to
     ${partnerName} with ${numberOfChildren} children`);
 }
-tellFortune('programmer','Tel Aviv','Sharon',2)
+tellFortune('programmer','Tel Aviv','Sharon',2);
