d17a0a84dd31b67f175af876dac7bb1094e42ed4
LiatBarak315 <li.lev315@yahoo.co.il>
2021-11-18 01:35:42 +0200

    finish

---
+++ Javascript/15.3-sum_of_even/15.3_sum_of_even.js
@@ -0,0 +1,4 @@
+function getSumOfEven(arr) {
+  return arr[1] + arr[3] + arr[5] + arr[7] + arr[9];//Change the place of taking the value to one less because in the array start counting from place 0
+}
+getSumOfEven([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
