823da8b42dea9b596dd25fd203c65dbaed608793
LiatBarak315 <li.lev315@yahoo.co.il>
2021-11-18 02:47:45 +0200

    finish

--- Javascript/2.4_date/2.4_date.js
+++ Javascript/2.4_date/2.4_date.js
@@ -1,4 +1,32 @@
-function date(sameDay){
-    let sameDay = new Date(today)
-    console.log(sameDay)
-}
+const getCurrentDate = () => {
+  const d = new Date();
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const dayName = days[d.getDay()];
+  const day = d.getDay();
+  const month = months[d.getMonth()];
+  const year = d.getFullYear();
+  console.log(`Today is ${dayName} the ${day} of ${month}, ${year} `);
+};
+getCurrentDate();
