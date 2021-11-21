6be2357b7fa1450babd3ba8d948c2200d2aa0226
LiatBarak315 <li.lev315@yahoo.co.il>
2021-11-21 05:42:49 +0200

    finish

---
+++ Javascript/JS_Weekend_Excercises2/‏‏Ex6.4_Isogram.js
@@ -0,0 +1,8 @@
+const isogram = (str) => {
+  let t=str.split("").every((c, i) => str.indexOf(c) == i);
+return t;
+}
+console.log(isogram("agffhmnjnm"));
+console.log(isogram("axsertyuiop"));
+console.log(isogram("aba"));
+console.log(isogram("agfnm"));
