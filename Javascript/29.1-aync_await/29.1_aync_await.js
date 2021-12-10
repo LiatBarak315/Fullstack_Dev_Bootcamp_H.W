a602e1a32c32319586a19a7265bce310ee0a46ac
LiatBarak315 <li.lev315@yahoo.co.il>
2021-12-10 14:17:00 +0200

    finish

---
+++ Javascript/29.1-aync_await/29.1_aync_await.js
@@ -0,0 +1,46 @@
+const getIDs = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([532, 543, 753, 1, 5]);
+    }, 1500);
+  });
+//get the rechip
+const getRecipe = (recipeID) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(
+      (ID) => {
+        const recipe = {
+          title: "Fresh tomato pasta",
+          publisher: "Pinchas Hodadad",
+        };
+        resolve(`${ID}: ${recipe.title}`);
+      },
+      1500,
+      recipeID
+    );
+  });
+};
+
+// getIDs()
+//   .then((IDs) => {
+//     console.log(IDs);
+//     return getRecipe(IDs[2]);
+//   })
+//   .then((recipe) => {
+//     console.log(recipe);
+//   })
+//   .catch((error) => {
+//     console.log("It is an error!");
+//   });
+const getRec=async()=>{
+    try{
+        const ids=await getIDs();
+        const recs=await getRecipe(ids[2]);
+        console.log(ids);
+        console.log(recs);
+    }
+    catch(err){
+        console.log("It is an error!",err);
+    }
+}
+getRec();
